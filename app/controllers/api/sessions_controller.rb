82a877c4a3e59f3a20e6ac7e14f9974d81768fd2
owenshaupt <owenshaupt@gmail.com>
2019-12-02 17:07:16 -0800

    login and logout working

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::SessionsController < ApplicationController
   def create
-    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    @user = User.find_by_credentials(params[:email], params[:password])
     if @user.nil?
       render json: ['Invalid email or password. Please try again'], status: 422
     else
@@ -13,10 +13,10 @@ class Api::SessionsController < ApplicationController
   def destroy
     if !current_user
       render json: ['No user to log out!'], status: 404
+    else
+      logout!
+      render json: ['Logged out successfully']
     end
-
-    logout!
-    render json: [{}]
   end
 end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  skip_before_action :verify_authenticity_token
   helper_method :current_user, :logged_in?
 
   def login!(user)
@@ -6,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def logout!
-    current_user.reset_session_token!
+    current_user.reset_session_token! if current_user
 
     session[:session_token] = nil
   end
