5ce14cd2608ff44c35a3f2e15253e9c264689a7c
owenshaupt <owenshaupt@gmail.com>
2019-12-02 20:16:55 -0800

    refactored to include App.jsx and root.jsx

--- frontend/caterhub.jsx
+++ frontend/caterhub.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {signup, login, logout} from './util/session_api_util'
+// import { signup, login, logout } from "./util/session_api_util";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
 
+  // TESTING START
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
+  // window.signup = signup;
+  // window.login = login;
+  // window.logout = logout;
 
-  window.signup = signup;
-  window.login = login;
-  window.logout = logout;
+  // TESTING END
 
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to Caterhub</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
