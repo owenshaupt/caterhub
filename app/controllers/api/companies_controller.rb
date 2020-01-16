b48acf56fbf03d13422ef65c6765e60fc8f27835
owenshaupt <owenshaupt@gmail.com>
2020-01-15 16:31:50 -0800

    added company controller and view/jbuilder

--- app/controllers/api/companies_controller.rb
+++ app/controllers/api/companies_controller.rb
@@ -1,2 +1,14 @@
 class Api::CompaniesController < ApplicationController
+  def show
+    @user = User.find_by(company_string: company_string)
+    render :show
+  end
+  
+  private
+
+  def company_params
+    params.permit(
+      :company_string
+    )
+  end
 end
